a5e971d6e880e7a9e36f1981176525b4e9cf105c
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2026-01-21 22:29:38 +0100

    chore: 🚧 Polygon drawing, finally

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import {useEffect, useRef} from "react";
-import {Canvas, Circle, type FabricObject, type TPointerEventInfo} from "fabric";
+import {Canvas, Circle, controlsUtils, type FabricObject, Polygon, Polyline, type TPointerEventInfo} from "fabric";
 
 export default function App() {
     const shape = useRef<FabricObject | undefined>(undefined);
@@ -20,10 +20,66 @@ export default function App() {
         canvas.requestRenderAll();
     };
 
+    const handleMouseDownPolyLine = (canvas: Canvas, event: TPointerEventInfo) => {
+        if (shape.current) {
+            const pointer = event.scenePoint;
+            const line = shape.current as Polyline;
+            line.points.push({x: pointer.x, y: pointer.y});
+            canvas.requestRenderAll();
+        } else {
+            const pointer = event.scenePoint;
+            shape.current = new Polygon([{x: pointer.x, y: pointer.y}, {x: pointer.x, y: pointer.y}], {
+                stroke: 'green',
+                selectable: true,
+                hasControls: true,
+                objectCaching: false,
+                strokeWidth: 2,
+            });
+            canvas.add(shape.current);
+            canvas.requestRenderAll();
+        }
+    }
+
+    const handleMouseMovePolyLine = (canvas: Canvas, event: TPointerEventInfo) => {
+        if (!shape.current) {
+            return;
+        }
+        const pointer = event.scenePoint;
+        const line = shape.current as Polyline;
+        line.points[line.points.length - 1].x = pointer.x;
+        line.points[line.points.length - 1].y = pointer.y;
+        line.dirty = true;
+        canvas.requestRenderAll();
+    };
+
+    const handleMouseDblClickPolyLine = (canvas: Canvas, disposer: VoidFunction) => {
+        const poly = shape.current as Polygon;
+        console.log('FINAL POLY POINTS:', poly.points);
+        poly.setCoords();
+        canvas.remove(poly);
+        const poly2 = new Polygon(poly.points.slice(0, poly.points.length - 2), {
+            stroke: 'blue',
+            selectable: true,
+            hasControls: true,
+            objectCaching: false,
+            strokeWidth: 4,
+        });
+        poly2.cornerStyle = 'circle';
+        poly2.cornerColor = 'rgba(0,0,255,0.5)';
+        poly2.hasBorders = false;
+        poly2.controls = controlsUtils.createPolyControls(poly);
+        canvas.add(poly2);
+        shape.current = undefined;
+        disposer();
+    }
+
     useEffect(() => {
         const canvas = new Canvas("myCanvas");
-        canvas.setDimensions({width: 400, height: 300});
-        canvas.on("mouse:down", (e) => handleMouseDownCircle(canvas, e));
+        canvas.setDimensions({width: 800, height: 600});
+        // canvas.on("mouse:down", (e) => handleMouseDownCircle(canvas, e));
+        const mouseDownDisposer = canvas.on("mouse:down", (e) => handleMouseDownPolyLine(canvas, e));
+        canvas.on('mouse:move', (e) => handleMouseMovePolyLine(canvas, e));
+        canvas.on('mouse:dblclick', (e) => handleMouseDblClickPolyLine(canvas, mouseDownDisposer));
         return () => {
             canvas.dispose();
         }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const container = document.getElementById('root') as Element;
+const root = createRoot(container);
+root.render(<App/>);
