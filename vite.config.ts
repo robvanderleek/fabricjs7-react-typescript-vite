a0b95700c6dc4d3cdef881eda42fc88b9ed31a61
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2026-02-06 16:34:28 +0100

    ci: 💚 Fix build

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    base: "/fabricjs-react-typescript-vite/",
+    base: "/fabricjs7-react-typescript-vite/",
     plugins: [react()],
     server: {
         open: true
